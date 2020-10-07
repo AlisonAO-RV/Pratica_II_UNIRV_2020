789b3d063f7381ebf6d7beebe57c7fda3b1b3093
AlisonAO-RV <alisonrv@msn.com>
2020-10-07 20:18:18 -0300

    Exercicios Pratica II

--- Provas/Prova_01/prova_04.java
+++
@@ -1,4 +0,0 @@
-// Sobre vetores e matrizes é correto o que se afirma, EXCETO em:
-
-// Os vetores ou matrizes podem armazenar quantidades variadas de elementos de um mesmo tipo, ou seja, não é necessário determinar o tamanho deles.
-
